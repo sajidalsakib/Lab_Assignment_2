58c681f30dff10252ac3a5d818df43ccf2bdea98
sajidalsakib <sajidalsakib@users.noreply.github.com>
2021-03-06 08:15:58 -0500

    Open function

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -31,6 +31,63 @@ namespace Assignment2
                 switch (x)
                 {
                     case "open":
+                        int src = 0;
+
+                        while (true)
+                        {
+                            Console.Write("savings : Open a savings account\nchecking : Open a checking account\nquit : Exit the application\nInput Your choice : ");
+                            string inp = Console.ReadLine();
+                            switch (inp)
+                            {
+                                case "savings":
+                                    Console.Write("Enter Account : \nName : ");
+                                    string name = Console.ReadLine();
+                                    Console.Write("Date of Birth : ");
+                                    string dob = Console.ReadLine();
+                                    Console.Write("Balance : ");
+                                    double balance = Convert.ToDouble(Console.ReadLine());
+                                    Console.Write("Address :\nRoad No. : ");
+                                    string r = Console.ReadLine();
+                                    Console.Write("House No. : ");
+                                    string h = Console.ReadLine();
+                                    Console.Write("City : ");
+                                    string c = Console.ReadLine();
+                                    Console.Write("Country : ");
+                                    string co = Console.ReadLine();
+                                    int id = AutoIdGenerator();
+                                    Account a1;
+                                    a1 = new SavingAccount(id,name, dob, balance, new Address(r, h, c, co));
+                                    bank.AddAccount(a1);
+                                    break;
+                                case "checking":
+                                    Console.Write("Enter Account : \nName : ");
+                                    string name1 = Console.ReadLine();
+                                    Console.Write("Date of Birth : ");
+                                    string dob1 = Console.ReadLine();
+                                    Console.Write("Balance : ");
+                                    double balance1 = Convert.ToDouble(Console.ReadLine());
+                                    Console.Write("Address :\nRoad No. : ");
+                                    string r1 = Console.ReadLine();
+                                    Console.Write("House No. : ");
+                                    string h1 = Console.ReadLine();
+                                    Console.Write("City : ");
+                                    string c1 = Console.ReadLine();
+                                    Console.Write("Country : ");
+                                    string co1 = Console.ReadLine();
+                                    int id1 = AutoIdGenerator();
+                                    Account a2;
+                                    a2 = new SavingAccount(id1, name1, dob1, balance1, new Address(r1, h1, c1, co1));
+                                    bank.AddAccount(a2);
+                                    break;
+                                case "quit":
+                                    src = 1;
+                                    break;
+                                default:
+                                    break;
+                            }
+                            if (src == 1) break;
+
+                        }
 
                         break;
                     case "account":
