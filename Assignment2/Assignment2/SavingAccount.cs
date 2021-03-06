ecf9563bc72803277743e6304f56d06ed892fc51
sajidalsakib <sajidalsakib@users.noreply.github.com>
2021-03-06 07:34:46 -0500

    Adding SavingAccount class

--- Assignment2/Assignment2/SavingAccount.cs
+++ Assignment2/Assignment2/SavingAccount.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    class SavingAccount
+    class SavingAccount : Account
     {
+        public SavingAccount(int accountNumber,string accountName, string dateOfBirth, double balance, Address address) 
+        {
+            base.accountName = accountName;
+            base.accountNumber = accountNumber;
+            base.dateOfBirth = dateOfBirth;
+            base.balance = balance;
+            base.address = address;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (amount > 0 && amount < base.balance)
+            {
+                Console.WriteLine("Previous Balance : {0}\ntWithdraw Amount: {1}", base.balance, amount);
+                base.balance = base.balance-amount;
+                Console.WriteLine("Current Balance: " + base.balance);
+                base.transactions++;
+            }
+            else Console.WriteLine("Withdraw unsucessful.....");
+        }
+        public override void Transfer(Account receiver, double amount)
+        {
+            if (amount > 0 && amount < base.balance)
+            {
+                Console.WriteLine("Previous Balance : {0}\nTransfer Amount: {1}", base.balance, amount);
+                base.balance = base.balance-amount;
+                receiver.Balance = receiver.Balance + amount;
+                Console.WriteLine("Current Balance: " + base.balance);
+                base.transactions++;
+            }
+            else Console.WriteLine("Transfer Unsucessful.....");
+        }
     }
 }
