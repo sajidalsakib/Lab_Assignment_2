7802c15a08f69b299140bfac429d0033009d0f79
sajidalsakib <sajidalsakib@users.noreply.github.com>
2021-03-06 08:05:03 -0500

    Adding Switch Case

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -18,6 +18,33 @@ namespace Assignment2
 
         static void Main(string[] args)
         {
+            Bank bank = new Bank();
+            int check = 0;
+
+
+            while (true)
+            {
+        
+                Console.Write("open : Open a Bank account\naccount : Perform transactions on an account\nquit : Exit the application\nInput Your choice : ");
+                string x = Console.ReadLine();
+
+                switch (x)
+                {
+                    case "open":
+
+                        break;
+                    case "account":
+                        break;
+                    case "quit":
+                        check = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+
+                }
+                if (check == 1) break;
+            }
         }
     }
 }
